ec16e66f721a7823c4109a7442609f058a1c58eb
Matheus Ferro <62824398+Matheus-Ferro@users.noreply.github.com>
2023-07-12 12:06:15 -0400

    Fix Typo

--- aula4_heranca/src/empresa/Pricipal.java
+++ aula4_heranca/src/empresa/Pricipal.java
@@ -5,12 +5,12 @@ public class Pricipal {
 	public static void main(String[] args) {
 		
 //		l1 é uma instancia de Livro
-		Livro l1 = new Livro("A Revolta de Atras", "Ayn Rand");
+		Livro l1 = new Livro("A Revolta de Atlas", "Ayn Rand");
 		l1.imposto();
 		
 //		l2 é uma instancia de Livro e LivroDigital, por LivroDigital ser uma classe filha
 //		de livro.
-		LivroDigitalYes l2 = new LivroDigitalYes("A Revolta de Atras", "Ayn Rand");
+		LivroDigitalYes l2 = new LivroDigitalYes("A Revolta de Atlas", "Ayn Rand");
 		l2.imposto();
 	}
 
